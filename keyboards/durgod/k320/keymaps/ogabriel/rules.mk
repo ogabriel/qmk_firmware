70082fbe5dde669ef560fa8bc59ef886ed021c82
ogabriel <gabrieloliver8991@gmail.com>
2023-05-01 15:45:32 -0300

    add new keymap for k320

---
+++ keyboards/durgod/k320/keymaps/ogabriel/rules.mk
@@ -0,0 +1,18 @@
+# Build Options
+#   change yes to no to disable
+#
+
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+MUSIC_ENABLE = no
+LTO_ENABLE = yes
+
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+
+VIA_ENABLE = yes
