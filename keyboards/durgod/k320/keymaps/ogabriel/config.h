106cd1a91188bedac454c586eca58e434da42ffa
ogabriel <gabrieloliver8991@gmail.com>
2023-05-02 08:43:51 -0300

    add mouse settings

--- keyboards/durgod/k320/keymaps/ogabriel/config.h
+++ keyboards/durgod/k320/keymaps/ogabriel/config.h
@@ -25,3 +25,18 @@
 #define FORCE_NKRO
 #define USB_POLLING_INTERVAL_MS 1
 #define QMK_KEYS_PER_SCAN 10
+
+// mouse settings
+#define MK_KINETIC_SPEED                        // Enable Kinetic mode:  Uses a quadratic curve on cursor speed to allow precise movements at the beginning and increases speed thereafter.
+#define MOUSEKEY_DELAY 3                        // Delay between pressing a movement key and cursor movement (default: 10)
+#define MOUSEKEY_INTERVAL 13                    // Time between cursor movements in milliseconds (default: 20); Try setting to 1000/monitor refresh for smooth movement.
+#define MOUSEKEY_MOVE_DELTA 5                   // Step size for accelerating from initial to base speed (default: 8)
+#define MOUSEKEY_MOVE_MAX 50                    // use instead of BASE SPEED to limit speed in Kinetic mode
+#define MOUSEKEY_INITIAL_SPEED 100              // Initial speed of the cursor in pixels per second (default: 100)
+//#define MOUSEKEY_BASE_SPEED 800               // (broken in QMK 0.16.0) Maximum cursor speed at which acceleration stops (default: 1000)
+#define MOUSEKEY_DECELERATED_SPEED 400          // Decelerated cursor speed (default: 400)
+#define MOUSEKEY_ACCELERATED_SPEED 2000         // Accelerated cursor speed (default: 3000)
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 16     // Initial number of movements of the mouse wheel (default: 16)
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS 32        // Maximum number of movements at which acceleration stops (default: 32)
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48 // Accelerated wheel movements (default: 48)
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8  // Decelerated wheel movements (default: 8)
