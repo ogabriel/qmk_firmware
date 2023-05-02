877d3a6be58ac626415ff3785d45777c6a7af7b8
ogabriel <gabrieloliver8991@gmail.com>
2023-05-02 08:51:06 -0300

    add more comments

--- keyboards/durgod/k320/keymaps/ogabriel/rules.mk
+++ keyboards/durgod/k320/keymaps/ogabriel/rules.mk
@@ -2,6 +2,7 @@
 #   change yes to no to disable
 #
 
+# firmware savings settings
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
