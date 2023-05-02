306eab123a1195aa1d07591beea8249d087c6e77
ogabriel <gabrieloliver8991@gmail.com>
2023-05-02 08:50:47 -0300

    separate gamer settings

--- keyboards/durgod/k320/keymaps/ogabriel/rules.mk
+++ keyboards/durgod/k320/keymaps/ogabriel/rules.mk
@@ -13,8 +13,9 @@ LTO_ENABLE = yes
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-NKRO_ENABLE = yes           # Enable N-Key Rollover
 
 VIA_ENABLE = yes
 
+# gamer settings
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 DEBOUNCE_TYPE = sym_defer_pk
